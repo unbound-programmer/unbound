61a5209ce7f412ea6809ff0792f4a94b8b705ef3
unbound.programmer <unbound.programmer@gmail.com>
2021-01-06 00:54:49 +0600

    First commit

---
+++ spring/unbound/src/main/java/unbound/UnboundApplication.java
@@ -0,0 +1,13 @@
+package unbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnboundApplication.class, args);
+	}
+
+}
---
+++ spring/unbound/src/test/java/unbound/UnboundApplicationTests.java
@@ -0,0 +1,13 @@
+package unbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
