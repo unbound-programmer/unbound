953101557f25fadf7f0571876db6bf7b9c07617e
unbound-programmer <unbound.programmer@gmail.com>
2021-02-24 16:51:44 +0600

    route successfull into tomcat deployment

--- angular/unbound/src/app/app.module.ts
+++ angular/unbound/src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  // providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
